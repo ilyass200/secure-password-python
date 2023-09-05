29e884648d38305119101e2494428c9fc7b9f7c1
ilyass200 <ajdainii@gmail.com>
2023-09-05 15:59:21 +0200

    ajouter le formulaire + controlleur

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # secure-password-python
+
+# bugs rencontrés
+ - Recuperer les données sous une route lors de la validation du formulaire 
--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, request, render_template
+import utils.generate_password
 
 app = Flask(__name__)
 
@@ -6,5 +7,16 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/generate-password', methods=['get', 'post'])
+def generatePassword():
+    if request.method == 'post':
+        print(request.form)
+        password_length = int(request.form.get('password_length'))
+        include_numbers = request.form.get('include_numbers') == 'true'
+        include_symbols = request.form.get('include_symbols') == 'true'
+        
+        return render_template('index.html')
+    return "error"
+
 if __name__ == '__main__':
-    app.run()
+    app.run(port=8080)
