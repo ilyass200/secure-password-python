2c63c0a6c3e6b05546a2f0f62bef7a6c2712b11b
yloukou <yloukou@solocal.com>
2023-09-06 16:11:10 +0200

    modif functions

--- app/utils/generate_password.py
+++ app/utils/generate_password.py
@@ -3,15 +3,32 @@ import string
 import hashlib
 
 
-def generate_password(length, use_digits=False, use_symbols=False):
+def generate_password(taille, use_digits=False, use_symbols=False):
+    # Vérifier que la longueur est un entier compris entre 8 et 50
+    if not (isinstance(taille, int) and 8 <= taille <= 50):
+        raise ValueError("La longueur doit être un entier entre 8 et 50 inclus.")
+
     characters = string.ascii_letters
     if use_digits:
         characters += string.digits
     if use_symbols:
         characters += string.punctuation
 
-    password = ''.join(random.choice(characters) for _ in range(length))
-    return password
+    # Si l'utilisateur veut un chiffre, ajouter un chiffre aléatoire au mot de passe
+    if use_digits:
+        characters += random.choice(string.digits)
+
+    # Si l'utilisateur veut un caractère de ponctuation, ajouter un caractère de ponctuation aléatoire
+    if use_symbols:
+        characters += random.choice(string.punctuation)
+
+    # Générer le reste du mot de passe
+    password = ''.join(random.choice(characters) for _ in range(taille - 2))  # Soustraire 2 pour les caractères ajoutés ci-dessus
+
+    # Mélanger les caractères pour garantir l'aléatoire
+    password_list = list(password)
+    random.shuffle(password_list)
+    return ''.join(password_list)
 
 
 def hash_password(password, hash_algorithm):
@@ -28,11 +45,11 @@ def hash_password(password, hash_algorithm):
 
 
 if __name__ == "__main__":
-    length = int(input("Entrez la longueur du mot de passe : "))
+    taille = int(input("Entrez la longueur du mot de passe : "))
     use_digits = input("Inclure des chiffres ? (Oui/Non) : ").strip().lower() == "oui"
     use_symbols = input("Inclure des symboles ? (Oui/Non) : ").strip().lower() == "oui"
 
-    password = generate_password(length, use_digits, use_symbols)
+    password = generate_password(taille, use_digits, use_symbols)
     print("Mot de passe généré :", password)
 
     hash_choice = input("Voulez-vous hasher le mot de passe ? (Oui/Non) : ").strip().lower()
